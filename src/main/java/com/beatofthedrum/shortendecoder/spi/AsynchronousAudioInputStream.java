851c5bb61311201d37e82f32420663624f4f7cce
Karstian Lee <tianscar@protonmail.com>
2023-05-15 11:10:45 +0800

    Bug fix

--- src/main/java/com/beatofthedrum/shortendecoder/spi/AsynchronousAudioInputStream.java
+++ src/main/java/com/beatofthedrum/shortendecoder/spi/AsynchronousAudioInputStream.java
@@ -2,10 +2,13 @@ package com.beatofthedrum.shortendecoder.spi;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 import java.io.IOException;
 import java.io.InputStream;
 
 abstract class AsynchronousAudioInputStream extends AudioInputStream implements CircularBuffer.Trigger {
+
+	private static final int MAX_SKIP_BUFFER_SIZE = 2048;
 
 	private byte[] singleByte;
 	protected final CircularBuffer buffer;
@@ -35,6 +38,65 @@ abstract class AsynchronousAudioInputStream extends AudioInputStream implements 
 	@Override
 	public int read(byte[] b, int off, int len) throws IOException {
 		return buffer.read(b, off, len);
+	}
+
+	/**
+	 * Skips over and discards a specified number of bytes from this
+	 * audio input stream.
+	 * @param n the requested number of bytes to be skipped
+	 * @return the actual number of bytes skipped
+	 * @throws IOException if an input or output error occurs
+	 * @see #read
+	 * @see #available
+	 */
+	public long skip(long n) throws IOException {
+
+		// make sure not to skip fractional frames
+		if( (n%frameSize) != 0 ) {
+			n -= (n%frameSize);
+		}
+
+		if( frameLength != AudioSystem.NOT_SPECIFIED ) {
+			// don't skip more than our set length in frames.
+			if( (n/frameSize) > (frameLength-framePos) ) {
+				n = (frameLength-framePos) * frameSize;
+			}
+		}
+		long temp = mSkip(n);
+
+		// if no error, update our position.
+		if( temp%frameSize != 0 ) {
+
+			// Throw an IOException if we've skipped a fractional number of frames
+			throw new IOException("Could not skip an integer number of frames.");
+		}
+		if( temp >= 0 ) {
+			framePos += temp/frameSize;
+		}
+		return temp;
+
+	}
+
+	private long mSkip(long n) throws IOException {
+
+		long remaining = n;
+		int nr;
+
+		if (n <= 0) {
+			return 0;
+		}
+
+		int size = (int) Math.min(MAX_SKIP_BUFFER_SIZE, remaining);
+		byte[] skipBuffer = new byte[size];
+		while (remaining > 0) {
+			nr = read(skipBuffer, 0, (int)Math.min(size, remaining));
+			if (nr < 0) {
+				break;
+			}
+			remaining -= nr;
+		}
+
+		return n - remaining;
 	}
 
 	@Override
